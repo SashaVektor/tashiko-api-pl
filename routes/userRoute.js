be9b4350b243eca1d73fdbea5a0339dba54700b0
Nikita Biichuk <niktestpython@gmail.com>
2026-08-05 12:31:25 +0300

    feat: implement customer management features with input normalization, validation, and CRUD operations for admin users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editUserInfo, getAdminCustomerProfile, getAdminCustomers, signIn, signUp } from '../controllers/UserController.js';
+import { createAdminCustomer, editUserInfo, getAdminCustomerProfile, getAdminCustomers, signIn, signUp, updateAdminCustomer } from '../controllers/UserController.js';
 import { isAdmin, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
@@ -9,7 +9,9 @@ userRouter.post('/signin', signIn);
 userRouter.post('/signup', signUp);
 
 userRouter.get('/admin/customers', isAuth, isAdmin, getAdminCustomers);
+userRouter.post('/admin/customers', isAuth, isAdmin, createAdminCustomer);
 userRouter.get('/admin/customers/:id', isAuth, isAdmin, getAdminCustomerProfile);
+userRouter.put('/admin/customers/:id', isAuth, isAdmin, updateAdminCustomer);
 
 userRouter.put('/:id', isAuth, editUserInfo);
 
